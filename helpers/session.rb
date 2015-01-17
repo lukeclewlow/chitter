4aac74e4e9ca5c8bafe768b508a6ea5cbf5eb2fa
Luke Clewlow <luke.clewlow@gmail.com>
2015-01-17 11:51:24 +0000

    added log in possibilities

--- helpers/session.rb
+++ helpers/session.rb
@@ -18,6 +18,12 @@ module SessionHelpers
 	click_button "Sign Up"
 	end
 
+	 def sign_in(email, password)
+    visit '/sessions/new'
+    fill_in 'email', :with => email
+    fill_in 'password', :with => password
+    click_button 'Sign in'
+  end
 
 
 end
